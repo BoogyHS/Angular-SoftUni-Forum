49946a4c40f4cf0817e13ac109910491505bdddf
Boogy83 <bozhidar.atanasov@abv.bg>
2020-11-10 12:55:34 +0200

    edit authguard created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './shared/home/home.component';
 
+import { AuthGuard } from './guards/auth.guard';
+
+import { HomeComponent } from './shared/home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ThemesComponent } from './themes/themes/themes.component';
 import { LoginComponent } from './user/login/login.component';
@@ -14,10 +16,10 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'themes', component: ThemesComponent },
   { path: 'themes/:themeId', component: ThemeContentComponent },
-  { path: 'add-theme', component: NewThemeComponent },
+  { path: 'add-theme', component: NewThemeComponent, canActivate: [AuthGuard] },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'profile', component: ProfileComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
   { path: '**', component: NotFoundComponent },
 ];
 
